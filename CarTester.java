9d2d8afaa153f93844762c6d9946384ba15cbd8a
Synth9 <96803186+Synth9@users.noreply.github.com>
2024-09-11 18:33:33 -0700

    Update CarTester.java
    Finished CarTester

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class CarTester{
+    public static Car addCar(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter the car's name: ");
+        String carName = input.nextLine();
+        
+        System.out.print("\nPlease enter the car's miles: ");
+        double carMiles = input.nextDouble();
+        
+        System.out.print("\nPlease enter the car's gallons: ");
+        double carGallons = input.nextDouble();
+        
+        Car myCar = new Car(carName, carMiles, carGallons);
+        return myCar;
+    }
+    
+    public static void main(String[] args){
+        Car car1 = addCar();
+        System.out.println(car1.toString());
+        System.out.println("\n------NEXT CAR-------------------------------------------\n");
+        Car car2 = addCar();
+        System.out.println(car2.toString());
+    }
+}
